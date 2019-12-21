75e687af3b314415eb415188fbad4ec86be52f15
Mayank Dhingra <dhingra.mayank@gmail.com>
2019-12-22 01:23:51 +0530

    Emails you date wise split of birthdays, anniversaries and kids birthdays in the current month

--- event_reminder.py
+++ event_reminder.py
@@ -1,6 +1,7 @@
-import smtplib
+import smtplib,re,operator,sys
 from openpyxl import load_workbook
 from datetime import datetime,date
+from io import StringIO
 
 
 workbook = load_workbook(filename=r"C:\Users\MDD\Desktop\python101\data_1.xlsx") #filepath
@@ -13,54 +14,54 @@ birthdays={}
 anniversaries={}
 kids_birthdays={}
 
+old_stdout = sys.stdout # Memorize the default stdout stream
+result= StringIO()
+
+sys.stdout = result
+
+def change_date_format(dt):
+        return re.sub(r'(\d{4})-(\d{1,2})-(\d{1,2})', '\\3-\\2-\\1', dt)
+
 for value in sheet.iter_rows(min_row=2,min_col=1,values_only=True):	
 	if(isinstance(value[2],datetime)):
 		if(value[2].date().month==date.today().month):
 			birthday_count+=1
-			print(value[2].date(),value[0])
 			birthdays.update({value[0]:value[2]})
+	
+
+	if(isinstance(value[3],datetime)):
+		if(value[3].date().month==date.today().month):
+			anniversary_count+=1
+			anniversaries.update({value[0]:value[3]})
+
+	if(isinstance(value[4],datetime)):
+		if(value[4].date().month==date.today().month):
+			kids_birthdays_count+=1
+			kids_birthdays.update({value[0]:value[4]})
+
+
+birthdays=dict(sorted(birthdays.items(), key=operator.itemgetter(0)))
 print(f"This month has {birthday_count} birthdays,")
-#print(birthdays)
+for key in birthdays:
+	print(key,'->',change_date_format(str(birthdays[key].date())))
+	b_message = str(key) + "->" 
+print("\n")
+anniversaries=dict(sorted(anniversaries.items(), key=operator.itemgetter(0)))
+print(f"This month has {anniversary_count} anniversaries,")
+for key in anniversaries:
+	print(key,'->',change_date_format(str(anniversaries[key].date())))
+print("\n")
+kids_birthdays=dict(sorted(kids_birthdays.items(), key=operator.itemgetter(0)))
+print(f"This month has {kids_birthdays_count} kids_birthdays,")
+for key in kids_birthdays:
+	print(key,'->',change_date_format(str(kids_birthdays[key].date())))
+print("\n")
 
-	# birthday_count=0
-	# if(isinstance(value[2],datetime)):
-	# 	if(value[2].date().month==date.today().month):
-	# 		birthday_count+=1
-	# 		print(birthday_count)
-	# 		print(value[2].date())
-	# 		print(f"This month has {birthday_count} birthdays")	
-	# 	else:
-	# 		print(value)
-		# if(value[l].date().month==date.today().month):
-		# 	print(value[l].date())
-	# ifwhile i<len(value):
-	# 	print(value[i])
-	# 	i+=1
+sys.stdout = old_stdout # Put the old stream back in place
+whatWasPrinted = "\n" + result.getvalue() # Return a str containing the entire contents of the buffer.
 
+print(f"message is {whatWasPrinted}")
 
-
-
-
-
-
-	# l=0
-	# list={}
-	# while l<len(value):
-	# 	if(isinstance(value[l],datetime)):
-	# 		print(value[l])
-	# 		if(value[l].date().month==date.today().month):
-	# 			print(l)
-	# 			print(value)
-	# 			print(f"This month has birthdays on {value[l].date()}")
-	# 	l+=1
-				#print(f"This month birthdays for {value[0]} on {value[2].date()}")
-
-	#if(value[3] is not None and isinstance(value[3],datetime)):
-	#	if(value[3].date().month==date.today().month):
-	#		print(f"This month anniversary for {value[0]} on {value[3].date()}")
-	#if(value[4] is not None and isinstance(value[4],datetime)):
-	#	if(value[4].date().month==date.today().month):
-	#		print(f"This month kids birthdays for {value[0]} on {value[4].date()}")
-			
-
-
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.login("email id", "password") #email id and password
+server.sendmail("email id","receipient email id",message)
