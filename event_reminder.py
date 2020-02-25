689f7aa8aa529601546c56eaeeab5a17812b7fa0
mayankdhingra <dhingra.mayank@gmail.com>
2020-02-26 02:08:51 +0530

    made some tweaks to show this weeks birthday count

--- event_reminder.py
+++ event_reminder.py
@@ -14,13 +14,19 @@ kids_birthdays_count=0
 birthdays={}
 anniversaries={}
 kids_birthdays={}
+this_weeks_birthday_count = 0
+this_weeks_anniversary_count=0
+this_weeks_kids_birthdays_count=0
+this_weeks_birthdays={}
+this_weeks_anniversaries={}
+this_weeks_kids_birthdays={}
 next_month_birthday_count = 0
 next_month_anniversary_count=0
 next_month_kids_birthdays_count=0
 next_month_birthdays={}
 next_month_anniversaries={}
 next_month_kids_birthdays={}
-#months=['1':'Jan',]
+
 
 sender_email = "a@gmail.com"  # Enter your address
 receiver_email = "d@gmail.com"  # Enter receiver address
@@ -34,9 +40,24 @@ sys.stdout = result
 def change_date_format(dt):
         return re.sub(r'(\d{4})-(\d{1,2})-(\d{1,2})', '\\3-\\2-\\1', dt)
 
+def is_event_this_week(date_today,date_event):
+	day_since= (date_event-date_today).days
+	day_today=date_today.weekday()
+	map_this_week={0:[0,6],1:[-1,5],2:[-2,4],3:[-3,3],4:[-4,2],5:[-5,1],6:[-6,0]}
+	range=map_this_week[day_today]
+	if(day_since>=range[0] and day_since<=range[1]):
+		return True
+	else:
+		return False
+	
+
 for value in sheet.iter_rows(min_row=2,min_col=1,values_only=True):	
 	if(isinstance(value[2],datetime)):
 		if(value[2].date().month==date.today().month):
+			event_this_week_or_not=is_event_this_week(date.today(),value[2].date())
+			if event_this_week_or_not:
+				this_weeks_birthday_count+=1
+				#this_weeks_birthdays.update({value[0]:value[2]})
 			birthday_count+=1
 			birthdays.update({value[0]:value[2]})
 		if(date.today().month==12):
@@ -44,7 +65,7 @@ for value in sheet.iter_rows(min_row=2,min_col=1,values_only=True):
 				next_month_birthday_count+=1
 				next_month_birthdays.update({value[0]:value[2]})
 		else:
-			print("here")
+			#print("here")
 			if(value[2].date().month==date.today().month+1):
 				next_month_birthday_count+=1
 				next_month_birthdays.update({value[0]:value[2]})
@@ -74,13 +95,15 @@ for value in sheet.iter_rows(min_row=2,min_col=1,values_only=True):
 				next_month_birthday_count+=1
 				next_month_birthdays.update({value[0]:value[4]})
 		else:
-			print("here")
+			#print("here")
 			if(value[4].date().month==date.today().month+1):
 				next_month_birthday_count+=1
 				next_month_birthdays.update({value[0]:value[4]})
 
-print(f"This Month has {birthday_count} birthdays, {anniversary_count} anniversaries & {kids_birthdays_count} kids birthdays \n")
-
+#print(f"This Month has {birthday_count} birthdays, {anniversary_count} anniversaries & {kids_birthdays_count} kids birthdays \n")
+if this_weeks_birthday_count:
+	print(f"This week has {this_weeks_birthday_count} birthdays \n")
+	
 birthdays=dict(sorted(birthdays.items(), key=operator.itemgetter(1)))
 print(f"This month has {birthday_count} birthdays: \n")
 for key in birthdays:
@@ -89,7 +112,11 @@ for key in birthdays:
 print("\n")
 
 anniversaries=dict(sorted(anniversaries.items(), key=operator.itemgetter(1)))
-print(f"This month has {anniversary_count} anniversaries: \n")
+if anniversary_count:
+	print(f"This month has {anniversary_count} anniversaries: \n")
+else:
+	print(f"This month has No anniversaries: \n")
+	
 for key in anniversaries:
 	print(key,'->',change_date_format(str(anniversaries[key].date())))
 print("\n")
@@ -137,7 +164,6 @@ whatWasPrinted = message + result.getvalue() # Return a str containing the entir
 print(f"message is {whatWasPrinted}")
 
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-server.login("email id", "password") #email id and password
+#server.login("email id", "password") #email id and password
 #server.sendmail("email id","receipient email id",whatWasPrinted)
-
-server.sendmail(sender_email,receiver_email,whatWasPrinted)
+#server.sendmail(sender_email,receiver_email,whatWasPrinted)
